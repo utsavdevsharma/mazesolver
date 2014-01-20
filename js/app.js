babaa39b6c65ef5f3e451ec46c6da697d65ca726
utsavdevsharma <utsavdevsharma@gmail.com>
2014-01-20 21:24:28 +0530

    full working code, next steps are cleanup,tests,docs

--- js/app.js
+++ js/app.js
@@ -1,155 +1,280 @@
 function maze() {
+	this.entry = "[0,1]";
+	this.exit = "[4,3]";
 	this.mazeSize = 5;
 	this.travelledQueue = [];
 	this.prevMazeBox = null;
 	this.currentMazeBox = null;
 	this.motionDirections = ['top','right','bottom','left'];
+	this.initializedBoxes = [];
+	this.lastOneDiscarded = null;
+	this.stillHaveSomePaths = true;
 }
-maze.prototype.pushInQueue = function(prevMazeBox) {
+maze.prototype.pushInQueue = function() {
 	this.travelledQueue.push(this.prevMazeBox);
 };
 maze.prototype.popFromQueue = function() {
-	this.travelledQueue.pop();
+	this.lastOneDiscarded = this.travelledQueue.pop();
+	this.lastOneDiscarded.domElem.removeAttr('data-solved').attr('data-dormant','');
 };
 maze.prototype.markActiveBox = function(box) {
 	box.domElem.attr("data-checking","yes");
-	defMaze.currentMazeBox = box;
+	this.currentMazeBox = box;
 };
 maze.prototype.markChecked = function(box) {
 	box.domElem.removeAttr("data-checking");
 };
 maze.prototype.takeStep = function(box) {
-	defMaze.markChecked(box);
+	this.markChecked(box);
+	this.prevMazeBox = box;
+	this.pushInQueue();
 	box.domElem.attr("data-solved","yes");
 };
-var defMaze = new maze();
+var coreMaze = new maze();
 
 function getAboveBox(cords) {
 	var cordsArray = JSON.parse(cords);
 	cordsArray[1] = cordsArray[1]+1;
-	if (cordsArray[1]<defMaze.mazeSize) { return "["+cordsArray.toString()+"]"; } else { return null; }
+	if (cordsArray[1]<coreMaze.mazeSize) { return "["+cordsArray.toString()+"]"; } else { return null; }
 }
 
 function getBelowBox(cords) {
 	var cordsArray = JSON.parse(cords);
 	cordsArray[1] = cordsArray[1]-1;
-	if (cordsArray[1]>0) { return "["+cordsArray.toString()+"]"; } else { return null; }
+	if (cordsArray[1]>=0) { return "["+cordsArray.toString()+"]"; } else { return null; }
 }
 
 function getRightBox(cords) {
 	var cordsArray = JSON.parse(cords);
 	cordsArray[0] = cordsArray[0]+1;
-	if (cordsArray[0]<defMaze.mazeSize) { return "["+cordsArray.toString()+"]"; } else { return null; }
+	if (cordsArray[0]<coreMaze.mazeSize) { return "["+cordsArray.toString()+"]"; } else { return null; }
 }
 
 function getLeftBox(cords) {
 	var cordsArray = JSON.parse(cords);
 	cordsArray[0] = cordsArray[0]-1;
-	if (cordsArray[0]>0) { return "["+cordsArray.toString()+"]"; } else { return null; }
+	if (cordsArray[0]>=0) { return "["+cordsArray.toString()+"]"; } else { return null; }
 }
 
 function mazeBox(cords) {
-	// this.cords = JSON.parse(cords);
+	this.xy = cords;
 	this.domElem = $('[data-cords="'+cords+'"]');
 	this.open = this.domElem.attr('data-path-open');
-	this.aboveBoxCords = getAboveBox(cords);
+	this.aboveBox = getAboveBox(cords);
 	this.rightBox = getRightBox(cords);
 	this.belowBox = getBelowBox(cords);
 	this.leftBox = getLeftBox(cords);
+	this.openOptions = [];
+	this.restrictedFrom = [];
 }
 
 function moveInTRBL(box) {
-	defMaze.markActiveBox(box);
+	// verbose(box.xy);
+	coreMaze.markActiveBox(box);
 	traverseDirections:
-	for (var i = 0; i < defMaze.motionDirections.length; i++) {
-		var direction = defMaze.motionDirections[i];
-		if (direction=='top' && box.aboveBoxCords) {
-			var nextBox = new mazeBox(box.aboveBoxCords);
-			defMaze.markActiveBox(nextBox);
-			if (defMaze.currentMazeBox.open=="yes") {
-				defMaze.takeStep(box);
-				break traverseDirections;
+	for (var i = 0; i < coreMaze.motionDirections.length; i++) {
+		var direction = coreMaze.motionDirections[i];
+		var nextBox = undefined;
+		if (direction=='top' && box.aboveBox) {
+			if ( $('[data-cords="'+box.aboveBox+'"]').attr('data-obj') ) {
+				nextBox = coreMaze.initializedBoxes[$('[data-cords="'+box.aboveBox+'"]').attr('data-obj')]
 			} else {
-				defMaze.markChecked(nextBox);
-				defMaze.markActiveBox(box);
+				nextBox = new mazeBox(box.aboveBox); 
+			}
+		}
+		else if (direction=='right' && box.rightBox) {
+			if ( $('[data-cords="'+box.rightBox+'"]').attr('data-obj') ) {
+				nextBox = coreMaze.initializedBoxes[$('[data-cords="'+box.rightBox+'"]').attr('data-obj')]
+			} else {
+				nextBox = new mazeBox(box.rightBox); 
+			}
+		}
+		else if (direction=='bottom' && box.belowBox) {
+			if ( $('[data-cords="'+box.belowBox+'"]').attr('data-obj') ) {
+				nextBox = coreMaze.initializedBoxes[$('[data-cords="'+box.belowBox+'"]').attr('data-obj')]
+			} else {
+				nextBox = new mazeBox(box.belowBox); 
+			}
+		}
+		else if (direction=='left' && box.leftBox) {
+			if ( $('[data-cords="'+box.leftBox+'"]').attr('data-obj') ) {
+				nextBox = coreMaze.initializedBoxes[$('[data-cords="'+box.leftBox+'"]').attr('data-obj')]
+			} else {
+				nextBox = new mazeBox(box.leftBox); 
+			}
+		}
+		// console.log(nextBox,coreMaze.currentMazeBox,direction,coreMaze.travelledQueue.indexOf(nextBox));
+		if (nextBox != undefined) {
+			// console.log(nextBox);
+			// if (coreMaze.prevMazeBox) { console.log(coreMaze.prevMazeBox); } else { console.log(undefined); }
+			if ( coreMaze.currentMazeBox == box
+				&& coreMaze.travelledQueue.indexOf(nextBox) == -1
+				&& box.restrictedFrom.indexOf(nextBox) == -1 ) {
+				coreMaze.markActiveBox(nextBox);
+				// console.log(coreMaze);
+				if (coreMaze.currentMazeBox.open=="yes") {
+					coreMaze.takeStep(box);
+					verbose(nextBox.xy);
+					if ( nextBox.xy==coreMaze.exit ) {
+						clearTimeout(steps);
+						verbose('Solution found');
+						coreMaze.takeStep(nextBox);
+						outPut(coreMaze.travelledQueue);
+						break traverseDirections;
+					}
+					// console.log(coreMaze);
+				} else {
+					// console.log(coreMaze);
+					coreMaze.markChecked(nextBox);
+					coreMaze.markActiveBox(box);
+				}
+			} else {
+				if (nextBox.open=="yes" &&
+					coreMaze.travelledQueue.indexOf(nextBox) == -1 &&
+					box.restrictedFrom.indexOf(nextBox) == -1) {
+					box.openOptions.push(nextBox.xy);
+				}
+			}
+		}
+		if (i==coreMaze.motionDirections.length-1) {
+			if ( !(box.domElem.attr('data-obj')) ) {
+				box.domElem.attr('data-obj',coreMaze.initializedBoxes.length);
+				coreMaze.initializedBoxes.push(box);
+			}
+			if ( coreMaze.currentMazeBox == box ) {
+				clearTimeout(steps);
+				// console.log("clearTimeout");
+				coreMaze.takeStep(box);
+				verbose('This path is blocked, trying new path');
+				tryNewPath();
+			} else {
+				window.steps = setTimeout(stepper,800);
 			}
 		}
 	}
 }
 
+function stepper() {
+	moveInTRBL(coreMaze.currentMazeBox);
+}
+
+function tryNewPath() {
+	// coreMaze.popFromQueue();
+	traverseBack:
+	while(coreMaze.travelledQueue.length) {
+		coreMaze.stillHaveSomePaths = false;
+		var lastOne = coreMaze.travelledQueue[coreMaze.travelledQueue.length-1];
+		if( lastOne.openOptions.length ) {
+			// console.log("lastOne");
+			// console.log(coreMaze.lastOneDiscarded);
+			lastOne.restrictedFrom.push(coreMaze.lastOneDiscarded);
+			var delIndex = lastOne.openOptions.indexOf(coreMaze.lastOneDiscarded.xy);
+			// console.log(delIndex,lastOne.openOptions);
+			if (delIndex>-1) { lastOne.openOptions.splice(delIndex,1); }
+			coreMaze.stillHaveSomePaths = true;
+			moveInTRBL(lastOne);
+			break traverseBack;
+		} else {
+			// console.log("lastOnepoped");
+			coreMaze.popFromQueue();
+		}
+	};
+	if (!coreMaze.stillHaveSomePaths) {
+		clearTimeout(steps);
+		verbose('all possible paths blocked');
+		outPut('blocked');
+	}
+}
+
+function verbose(text) {
+	$('.js-verbose').append('<p>'+text+'</p>').scrollTop('1000');
+}
+
+function outPut(msg) {
+	$('.js-output').append('<p>'+msg+'</p>');
+}
+
+// ------------------------------------------------------------
 // Below code is for plotting diviers, not used in solving maze
+// ------------------------------------------------------------
 $('.js-go').click(function(){
 	var newDividersCords = $.trim($('.js-inp').val());
 	if ( newDividersCords == "" || newDividersCords == undefined ) {
-		alert("That input is not correct. Please use format [2,2]:[2,3];[2,2]:[3,2]");
+		verbose("That input is not correct. Please use format [2,2]:[2,3];[2,2]:[3,2]");
 	} else {
 		plotNewDividers(newDividersCords);
 	}
 });
 
+window.dividersCreated = 0
 function plotNewDividers(cords) { // input in format "[2,2]:[2,3];[2,2]:[3,2]"
-
 	var dividers = cords.split(";"); // dividers = ["[2,2]:[2,3]", "[2,2]:[3,2]"]
 	traverseDividers:
 	for (var i = dividers.length - 1; i >= 0; i--) {
 		// console.log("Plotting Divider "+dividers[i]);
+		var dividerEnds = dividers[i].split(":"); // dividerEnds = ["[2,2]", "[2,3]"]
+		var dividerStart = dividerEnds[0]; // "[2,2]"
+		var dividerEnd = dividerEnds[1]; // "[2,3]"
 
-			var dividerEnds = dividers[i].split(":"); // dividerEnds = ["[2,2]", "[2,3]"]
-			var dividerStart = dividerEnds[0]; // "[2,2]"
-			var dividerEnd = dividerEnds[1]; // "[2,3]"
-
-			if ( dividerEnds.length != 2 || $.trim(dividerStart) == "" || $.trim(dividerEnd) == "" ) {
-				alert("That input is not correct. Co-ordinates can not be blank and there can be only two set of them.");
+		if ( dividerEnds.length != 2 || $.trim(dividerStart) == "" || $.trim(dividerEnd) == "" ) {
+			verbose("That input is not correct. Co-ordinates can not be blank and there can be only two set of them.");
+			break traverseDividers;
+		} else {
+			try {
+				var dividerStartArray = JSON.parse(dividerStart); // "[2,2]" is now [2,2]
+				var dividerEndArray = JSON.parse(dividerEnd); // "[2,3]" is now [2,3]
+			}
+			catch (e) {
+				verbose("That input is not correct. Error in parsing Co-ordinates.");
+				break traverseDividers;
+			}
+			if ( dividerStartArray.length != 2 || dividerEndArray.length != 2 ) {
+				verbose("That input is not correct. There can not be more than two Co-ordinates in a set.");
 				break traverseDividers;
 			} else {
-				try {
-					var dividerStartArray = JSON.parse(dividerStart); // "[2,2]" is now [2,2]
-					var dividerEndArray = JSON.parse(dividerEnd); // "[2,3]" is now [2,3]
-				}
-				catch (e) {
-					alert("That input is not correct. Error in parsing Co-ordinates.");
-					break traverseDividers;
-				}
-				if ( dividerStartArray.length != 2 || dividerEndArray.length != 2 ) {
-					alert("That input is not correct. There can not be more than two Co-ordinates in a set.");
+				if ( dividerStartArray.some(checkPointValue) || dividerEndArray.some(checkPointValue) ) {
+					verbose("That input is not correct. Point value should be a number greater than or equal to zero and less than or equal to 4.");
 					break traverseDividers;
 				} else {
-					if ( dividerStartArray.some(checkPointValue) || dividerEndArray.some(checkPointValue) ) {
-						alert("That input is not correct. Point value should be a number greater than or equal to zero and less than or equal to 4.");
+					var inEqualX = dividerStartArray[0] != dividerEndArray[0];
+					var inEqualY = dividerStartArray[1] != dividerEndArray[1];
+					// console.log("inEqualX "+inEqualX+" inEqualY "+inEqualY);
+					if ( inEqualX && inEqualY ) {
+						verbose("That input is not correct. Diaognal or L shaped dividers are not possible.");
 						break traverseDividers;
 					} else {
-						var inEqualX = dividerStartArray[0] != dividerEndArray[0];
-						var inEqualY = dividerStartArray[1] != dividerEndArray[1];
-						// console.log("inEqualX "+inEqualX+" inEqualY "+inEqualY);
-						if ( inEqualX && inEqualY ) {
-							alert("That input is not correct. Diaognal or L shaped dividers are not possible.");
-							break traverseDividers;
+						if (inEqualY) { var traverseOnAxis = 1; } else { var traverseOnAxis = 0; }
+						// console.log("traverseOnAxis "+traverseOnAxis);
+						if (dividerStartArray[traverseOnAxis] < dividerEndArray[traverseOnAxis]){
+							var loopStart = dividerStartArray[traverseOnAxis];
+							var loopEnd = dividerEndArray[traverseOnAxis];
 						} else {
-							if (inEqualY) { var traverseOnAxis = 1; } else { var traverseOnAxis = 0; }
-							// console.log("traverseOnAxis "+traverseOnAxis);
-							if (dividerStartArray[traverseOnAxis] < dividerEndArray[traverseOnAxis]){
-								var loopStart = dividerStartArray[traverseOnAxis];
-								var loopEnd = dividerEndArray[traverseOnAxis];
+							var loopStart = dividerEndArray[traverseOnAxis];
+							var loopEnd = dividerStartArray[traverseOnAxis];
+						}
+						// console.log("loopStart "+loopStart);
+						// console.log("loopEnd "+loopEnd);
+						traverseBoxes:
+						for (var d = loopStart; d <= loopEnd; d++) {
+							// console.log(d);
+							if (inEqualY) {
+								blockPath("["+dividerStartArray[0]+","+d+"]");
 							} else {
-								var loopStart = dividerEndArray[traverseOnAxis];
-								var loopEnd = dividerStartArray[traverseOnAxis];
+								blockPath("["+d+","+dividerStartArray[1]+"]");
 							}
-							// console.log("loopStart "+loopStart);
-							// console.log("loopEnd "+loopEnd);
-							traverseBoxes:
-							for (var d = loopStart; d <= loopEnd; d++) {
-								// console.log(d);
-								if (inEqualY) {
-									blockPath("["+dividerStartArray[0]+","+d+"]");
-								} else {
-									blockPath("["+d+","+dividerStartArray[1]+"]");
-								}
-							}
+							if (d==loopEnd) { dividersCreated=dividersCreated+1; }
 						}
 					}
 				}
 			}
-	};
+		}
+		// console.log(dividersCreated,dividers.length);
+		if ( dividersCreated == dividers.length ) {
+			window.mz = new mazeBox(coreMaze.entry);
+			moveInTRBL(mz);
+			$('.js-go').attr('disabled','disabled')
+		}
+	}
 }
 
 function checkPointValue(element, index, array) {
@@ -161,5 +286,5 @@ function checkPointValue(element, index, array) {
 function blockPath(cords) {
 	// console.log(cords);
 	var selectorString = '[data-cords="'+cords+'"]'
-	$(selectorString).attr('data-divider','yes');
+	$(selectorString).attr('data-divider','yes').attr('data-path-open','no');
 }
